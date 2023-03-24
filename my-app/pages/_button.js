796e872edaae90b26d2d7263aff9fb4817eb75f5
Diego Gz <diegojgz@hotmail.com>
2023-03-24 00:02:19 -0300

    primer app con react

---
+++ my-app/pages/_button.js
@@ -0,0 +1,8 @@
+export default function Button() {
+    return(
+        <button>
+            Este es el primer botón.
+        </button>
+
+    )
+}
---
+++ my-app/pages/_video.js
@@ -0,0 +1,6 @@
+export default function Video() {
+    return(
+        <video width="320" height="240" controls src="https://www.w3schools.com/tags/movie.mp4" type="video/mp4">
+        </video>
+    )
+}
